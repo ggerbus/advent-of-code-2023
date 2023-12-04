6dc7a60a4ff0e0e2be048359065eae79e6b02be9
Goran Gerbus <goran.gerbus@gmail.com>
2023-12-04 16:17:13 +0100

    day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,87 +1,50 @@
 fun main() {
-    val cubes = mapOf(
-            "blue" to 14,
-            "red" to 12,
-            "green" to 13
-    )
 
-    fun addNumber(row: String, startIndex: Int, endIndex: Int, data: MutableList<Data>, rowIndex: Int) {
-        val number = row.substring(startIndex, endIndex + 1).toInt()
-        data.add(Data(rowIndex, -1, 'x', number, startIndex, endIndex))
-    }
+    fun readCards(input: List<String>) = input.map {
+        var str = it.substring(5)
+        var index = str.indexOf(":")
+        val cardId = str.substring(0, index).trim()
+        str = str.substring(index + 2)
 
-    fun extractData(input: List<String>, data: MutableList<Data>) {
-        input.mapIndexed { rowIndex, row ->
-            var startIndex = -1
-            row.toCharArray().mapIndexed { index, it ->
-                if (it.isDigit()) {
-                    if (startIndex < 0) {
-                        startIndex = index
-                    } else {
-                        if (index == row.length - 1) {
-                            addNumber(row, startIndex, index, data, rowIndex)
-                            startIndex = -1
-                        }
-                    }
-                } else {
-                    if (startIndex >= 0) {
-                        addNumber(row, startIndex, index - 1, data, rowIndex)
-                        startIndex = -1
-                    } else {
-                    }
+        index = str.indexOf(" | ")
+        var wining = str.substring(0, index).trim()
+        var my = str.substring(index + 2).trim()
 
-                    if (it != '.') {
-                        data.add(Data(rowIndex, index, it,-1, -1, -1))
-                    }
-                }
-            }
-        }
+        return@map Card(cardId.toInt(),
+                wining.split("\\s+".toRegex()).map { it.toInt() },
+                my.split("\\s+".toRegex()).map { it.toInt() })
     }
 
     fun part1(input: List<String>): Int {
-        val data = mutableListOf<Data>()
-        val partNumbers = mutableListOf<Data>()
-
-        extractData(input, data)
-
-        val simbols = data.filter { it.isSimbol() }
-        data.filter { it ->
-            it.isNumber()
-        }.map { num ->
-            simbols.forEach {
-                if(num.isPartNumber(it)){
-                    partNumbers.add(num)
-//                    num.println()
-                }
-            }
-
-        }
-        return partNumbers.sumOf { it.number }
-    }
-
-    fun part2(input: List<String>): Int {
-        val data = mutableListOf<Data>()
-        val partNumbers = mutableListOf<Data>()
-
-        extractData(input, data)
-
-        val nums = data.filter { it.isNumber() }
-        return data.filter {
-            it.isSimbol() && it.simbol == '*'
-        }.map { simbol ->
-            val parts = nums.filter { num->
-                num.isPartNumber(simbol)
-            }
-            if(parts.count() == 2){
-                return@map parts.map { it.number }.reduce { acc, next -> acc * next }
-            } else {
-                return@map 0
-            }
+        return readCards(input).map { card ->
+            val count = card.myNumbers.filter {
+                it in card.winingNumbers
+            }.count()
+            return@map Math.pow(2.0, count - 1.0 ).toInt()
         }.sum()
     }
 
-    val testInput = readInput("Day03_test")
-    val input = readInput("Day03")
+    fun part2(input: List<String>): Int {
+        val cards = readCards(input)
+        val cardArray = Array(cards.size){1}
+
+        cards.forEach {card ->
+            val count = card.myNumbers.filter {
+                it in card.winingNumbers
+            }.count()
+
+            for(i in 1..count){
+                if(card.num + i <= cards.size) {
+                    cardArray[card.num - 1 + i] += cardArray[card.num - 1]
+                }
+            }
+        }
+
+        return cardArray.sum()
+    }
+
+    val testInput = readInput("Day04_test")
+    val input = readInput("Day04")
 
     testInput.println()
     part1(testInput).println()
@@ -102,29 +65,10 @@ fun main() {
     part2(input).println()
 }
 
-data class Data(
-        val row: Int,
-        val simbolIndex: Int,
-        val simbol: Char,
-        val number: Int,
-        val startIndex: Int,
-        val endIndex: Int
+data class Card(
+        val num: Int,
+        val winingNumbers: List<Int>,
+        val myNumbers: List<Int>,
 ) {
-    fun isPartNumber(simbol: Data): Boolean {
-        if(simbol.row !in IntRange(row - 1, row + 1)){
-            return false
-        }
-        if(simbol.simbolIndex in IntRange(startIndex - 1, endIndex + 1)) {
-            return true
-        }
-        return false
-    }
 
-    fun isNumber(): Boolean{
-        return !isSimbol()
-    }
-
-    fun isSimbol(): Boolean {
-        return simbolIndex >= 0
-    }
 }
